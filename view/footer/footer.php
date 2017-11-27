15b673ff8fb494ed085f3c7107c0821f3ce2eacd
primopietro <Pietro.hyde@gmail.com>
2017-11-27 09:50:27 -0500

    ok

--- view/footer/footer.php
+++ view/footer/footer.php
@@ -13,61 +13,7 @@ if(isset( $_SESSION['currentItem'])){
         |========================
         -->
         <footer class="xt-footer">
-            <div class="container">
-                <div class="section-separator">
-                    <div class="row">
-                        <div class="col-md-3 col-sm-3">
-                            <div class="footer-widget footer-contact">
-                                <img src="<?php echo $addedString; ?>assets/images/flogo.png" alt="" class="img-responsive">
-                                <ul>
-                                    <li><i class="fa fa-mobile-phone"></i><a href="">+(1234) 456 7896</a></li>
-                                    <li><i class="fa fa-envelope-o"></i><a href="">info@xootheme.com</a></li>
-                                    <li>
-                                        <i class="fa fa-location-arrow"></i>
-                                       <address>Address: 42/1, dariapara road, New york city, New york. USA</address> 
-                                    </li>
-                                </ul>
-                            </div>
-                    </div>
-                        <div class="col-md-3 col-sm-3">
-                            <div class="footer-widget">
-                                <h4>My account</h4>
-                                <ul>
-                                    <li><a href=""><i class="fa fa-caret-right"></i>My account</a></li>
-                                    <li><a href=""><i class="fa fa-caret-right"></i>about us</a></li>
-                                    <li><a href=""><i class="fa fa-caret-right"></i>Shopping cart</a></li>
-                                    <li><a href=""><i class="fa fa-caret-right"></i>Checkout</a></li>
-                                </ul>
-                            </div>
-                    </div>
-                        <div class="col-md-3 col-sm-3">
-                            <div class="footer-widget">
-                                <h4>Custom Service</h4>
-                                <ul>
-                                    <li><a href=""><i class="fa fa-caret-right"></i>Shopping & return</a></li>
-                                    <li><a href=""><i class="fa fa-caret-right"></i>Seacure Shopping</a></li>
-                                    <li><a href=""><i class="fa fa-caret-right"></i>International Shopping </a></li>
-                                    <li><a href=""><i class="fa fa-caret-right"></i>Affiliate</a></li>
-                                    <li><a href=""><i class="fa fa-caret-right"></i>contact</a></li>
-                                </ul>
-                            </div>
-                    </div>
-                        <div class="col-md-3 col-sm-3">
-                            <div class="footer-widget">
-                                <h4>Information</h4>
-                                <ul>
-                                    <li><a href=""><i class="fa fa-caret-right"></i>Store location</a></li>
-                                    <li><a href=""><i class="fa fa-caret-right"></i>About our shop</a></li>
-                                    <li><a href=""><i class="fa fa-caret-right"></i>Our blog</a></li>
-                                    <li><a href=""><i class="fa fa-caret-right"></i>Secure Shopping</a></li>
-                                    <li><a href=""><i class="fa fa-caret-right"></i>Delivery information</a></li>
-                                </ul>
-                            </div>
-                    </div>
-                    </div>
-                </div>
-            </div>
-            <div class="clearfix"></div>
+            
             <div class="footer-middle bg-2">
                 <div class="container">
                     <div class="row section-separator">
