5bad23a46e4c13ed833fa317c4c6e3acc610716c
primopietro <Pietro.hyde@gmail.com>
2017-11-27 09:54:11 -0500

    ok

--- view/header/header.php
+++ view/header/header.php
@@ -100,50 +100,7 @@ if(isset( $_SESSION['currentItem'])){
                             </div>
                         </div>
                         <div class="col-md-1 col-sm-2 col-xs-2">
-                            <div class="xt-cart">
-                                <ul>
-                                    <li class="dropdown">
-                                      <a href="" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown">
-                                       <i class="fa flaticon-shopping-cart"></i>
-                                      </a>
-                                        <ul class="dropdown-menu xt-cart-items">
-                                            <li>
-                                                <a href="">
-                                                    <img src="<?php echo $addedString; ?>assets/images/4.jpg" alt="">
-                                                    <h3>Lipstick</h3>
-                                                    <span class="cart-price">$299</span>
-                                                </a>
-                                            </li>
-                                            <li>
-                                                <a href="">
-                                                    <img src="<?php echo $addedString; ?>assets/images/1.jpg" alt="">
-                                                    <h3>T-Shirt</h3>
-                                                    <span class="cart-price">$499</span>
-                                                </a>
-                                            </li>
-                                            <li>
-                                                <a href="">
-                                                    <img src="<?php echo $addedString; ?>assets/images/3.jpg" alt="">
-                                                    <h3>Shirt</h3>
-                                                    <span class="cart-price">$399</span>
-                                                </a>
-                                            </li>
-                                            <li>
-                                                <a href="" class="subtotal top-checkout">
-                                                    <h3>Subtotal : </h3>
-                                                    <span class="total-price">$999</span>
-                                                </a>
-                                            </li>
-                                            <li>
-                                                <a href="" class="process top-checkout">
-                                                    <h3>Process to Checkout </h3>
-                                                </a>
-                                            </li>
-                                        </ul>
-                                    </li>
-                                </ul>
-                                <span class="xt-item-count">8</span>
-                            </div>
+                           
                         </div>
                     </div>
                 </div>
